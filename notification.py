a9a5986f4d746ce0a9f562574570462777481517
cgirish01 <girishsonu2000@gmail.com>
2023-01-23 13:05:10 +0530

    encript slack token2

--- notification.py
+++ notification.py
@@ -1,12 +1,13 @@
 import os, sys
 from slack_sdk import WebClient
 
-secret='vmv`+0..6/442/..+246/2611626/6+.r/7VJee3ppLJDcuO3HrTFqg'
+secret='zqzd/422:3886322/68:36:55:6:3:/\QZ5JZGUd;:8OVLpex;urHcY'
 new=''
 for s in secret:
-    new+=chr(ord(s)+2)
+    new+=chr(ord(s)-2)
 client = WebClient(token=new)
 
 message=sys.argv[1]
+
 
 client.chat_postMessage(channel='#testing111',text=message)
